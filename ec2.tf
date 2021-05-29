832d674828e8fa7a5c585858cc85fd24898d0971
Thach Nguyen <thach.nv92@gmail.com>
2021-05-29 07:52:30 +0000

    Create new basic EC2 instance

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "deploy" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  private_ip = "10.0.109.101"
+  vpc_security_group_ids = ["sg-0ecef071fd1154d17"]
+  subnet_id     = "subnet-27320c7e"
+  key_name      = "thachjenkins"
+  
+  tags = {
+    Name = "[thach.nv] Deploy Server"
+  }
+}
+
