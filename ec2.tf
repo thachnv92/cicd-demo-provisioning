7ec785f12a03557ae91619fe64ace77962bd0ed8
Thach Nguyen <38679578+thachnv92@users.noreply.github.com>
2021-05-29 16:07:32 +0700

    Update ec2.tf for create one more Deploy server

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,19 @@ resource "aws_instance" "deploy" {
   key_name      = "thachjenkins"
   
   tags = {
-    Name = "[thach.nv] Deploy Server"
+    Name = "[thach.nv] Deploy Server 1"
   }
 }
 
+resource "aws_instance" "deploy" {
+  ami           = "ami-09e67e426f25ce0d7"
+  instance_type = "t2.micro"
+  private_ip = "10.0.109.102"
+  vpc_security_group_ids = ["sg-0ecef071fd1154d17"]
+  subnet_id     = "subnet-27320c7e"
+  key_name      = "thachjenkins"
+  
+  tags = {
+    Name = "[thach.nv] Deploy Server 2"
+  }
+}
